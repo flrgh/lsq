74b6f881098526bb88c31f0709d8199462cacdd5
Michael Martin <michael.joe.martin@gmail.com>
2015-05-20 13:08:25 -0700

    Rework tab completion

--- lsq_completion.sh
+++ lsq_completion.sh
@@ -1,12 +1,14 @@
 _list_queries(){
+    failed=0
     [[ -e $HOME/.lsqrc ]] && . $HOME/.lsqrc
-    [[ -z $LSQ_BASEDIR ]] && exit
-    [[ -d $LSQ_BASEDIR/queries ]] || exit
+    [[ -z $LSQ_BASEDIR ]] && echo "lsq error: no \$LSQ_BASEDIR is set--tab completion will not work"
+    [[ -d $LSQ_BASEDIR/queries ]] || echo "lsq error: query directory is missing--tab completion will not work"
     queries=$(/bin/ls $LSQ_BASEDIR/queries | grep -oE "^(.+)\.lql$" | sed 's/\.lql//')
 }
 
 _comp_queries(){
-    local cur prev opts
+    _list_queries
+    local cur prev #opts
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
@@ -15,5 +17,6 @@ _comp_queries(){
     return 0
 }
 
-_list_queries
-complete -F _comp_queries lsq
+if [[ $failed -ne 1 ]]; then
+    complete -F _comp_queries lsq
+fi
